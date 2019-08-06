c93090620c9090563f6d0e6094cf04fd714a93cb
Nils Andresen <nils@nils-andresen.de>
2019-08-06 22:30:44 +0200

    moved the old regex-based folding from declarative to programmatic

--- src/fs-lang/src/foldingProvider.ts
+++ src/fs-lang/src/foldingProvider.ts
@@ -7,20 +7,27 @@ import {
     FoldingRange } from 'vscode';
 
 export class FsFoldingProvider implements FoldingRangeProvider {
+
     provideFoldingRanges(document: TextDocument, context: FoldingContext, token: CancellationToken): ProviderResult<FoldingRange[]> {
         if(token.isCancellationRequested){
             return null;
         }
 
-        // Test: Immer drei Zeilen falten:
-        const folds = Math.floor(document.lineCount / 3);
+        const startExpression = new RegExp("^.*\\$CMS_(?!END)[^\\$]+\\$.*$");
+        const endExpression = new RegExp("^.*\\$CMS_END_[^\\$]+\\$.*$");
         const ranges:FoldingRange[] = [];
+        let foldingStartLine = -1;
 
-        for(let i =0; i<folds; i++){
-            let start = i * 3;
-            let end = start + 2;
-
-            ranges.push(new FoldingRange(start, end));
+        for(let i =0; i<document.lineCount; i++){
+            let line = document.lineAt(i).text;
+            if(foldingStartLine < 0 && startExpression.test(line)){
+                // set start-marker
+                foldingStartLine = i;
+            } else if(endExpression.test(line)){
+                // push fold & reset start-marker
+                ranges.push(new FoldingRange(foldingStartLine, i));
+                foldingStartLine = -1;
+            }
         }
 
         return ranges;
